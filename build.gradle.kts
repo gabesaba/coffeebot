16d589d45f07afd44e8fd70f74ff1ee39d4d33cd
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-03-21 01:39:40 -0400

    Gradle Files

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("com.discord4j:discord4j-core:3.0.13")
+}
+
+application {
+    mainClassName = "betbot.MainKt"
+}
