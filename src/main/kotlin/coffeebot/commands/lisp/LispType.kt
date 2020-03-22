1ccc82481942a87e62a23195f19fe99282a4555b
Gabe <gabesaba@gmail.com>
2020-03-22 19:40:53 -0400

    LispType: add method display; Change invoke to !cl

--- src/main/kotlin/coffeebot/commands/lisp/LispType.kt
+++ src/main/kotlin/coffeebot/commands/lisp/LispType.kt
@@ -2,20 +2,24 @@ package coffeebot.commands.lisp
 
 sealed class LispType {
     abstract fun type(): String
+
+    abstract fun display(): String
 }
 
 class Number(val num: Int): LispType() {
-    override fun toString(): String {
+    override fun display(): String {
         return num.toString()
     }
 
     override fun type(): String = "Number"
 }
 
-class Fn(private val fn: (List<Expression>, Env) -> LispType): LispType() {
+class Fn(private val name: String, private val fn: (List<Expression>, Env) -> LispType): LispType() {
     fun apply(exprs: List<Expression>, env: Env) = fn(exprs, env)
 
     override fun type(): String = "Fn"
+
+    override fun display() = "The Fn $name"
 }
 
 class TypeError(override val message: String): Exception()
