b96e8808d7b9e5bd9541b0cb5dd4c143f7ef0be9
Gabe <gabesaba@gmail.com>
2020-03-22 22:13:17 -0400

    CoffeeLisp: Define Define

--- src/main/kotlin/coffeebot/commands/lisp/Env.kt
+++ src/main/kotlin/coffeebot/commands/lisp/Env.kt
@@ -1,13 +1,20 @@
 package coffeebot.commands.lisp
 
-class Env {
-    private val registry = mutableMapOf(
-            "+" to add,
-            "-" to sub,
-            "*" to mul
-    )
+class Env(private val parent: Env? = null, private val registry: MutableMap<String, LispObject>) {
 
-    fun find(symbol: String): LispType? {
-        return registry[symbol]
+    fun find(symbol: String): LispObject? {
+        return registry[symbol.toLowerCase()]
     }
+
+    fun set(symbol: String, lispObject: LispObject) {
+        registry[symbol.toLowerCase()] = lispObject
+    }
+
+    fun isGlobalEnv() = parent == null
 }
+
+val globalEnv = Env(registry =  mutableMapOf(
+        "+" to add,
+        "-" to sub,
+        "*" to mul,
+        "define" to define))
