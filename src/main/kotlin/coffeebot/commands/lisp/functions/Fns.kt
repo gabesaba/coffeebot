4d24ae478989beba908a55fdab91de67739c1a32
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-03-23 03:27:35 -0400

    Move Nums to its own file

--- src/main/kotlin/coffeebot/commands/lisp/functions/Fns.kt
+++ src/main/kotlin/coffeebot/commands/lisp/functions/Fns.kt
@@ -1,44 +1,10 @@
 package coffeebot.commands.lisp.functions
 
-import coffeebot.commands.lisp.Env
-import coffeebot.commands.lisp.Expression
 import coffeebot.commands.lisp.Fn
 import coffeebot.commands.lisp.LispError
 import coffeebot.commands.lisp.LispUnit
-import coffeebot.commands.lisp.LispNumber
 import coffeebot.commands.lisp.Token
 import coffeebot.commands.lisp.TypeError
-
-fun getInts(exprs: List<Expression>, env: Env): Iterable<Int> {
-    return exprs.map {
-        val v = it.eval(env)
-        if (v is LispNumber) {
-            v.num
-        } else {
-            throw TypeError("Expected num")
-        }
-    }
-}
-
-val add1 = Fn("add1") { args, env ->
-    valArgs(args, 1, "add1")
-
-    when (val res = args[0].eval(env)) {
-        is LispNumber -> LispNumber(res.num + 1)
-        else -> throw TypeError("add1 expected Num")
-    }
-}
-
-val sub1 = Fn("sub1") { args, env ->
-    when (val res = args[0].eval(env)) {
-        is LispNumber -> LispNumber(res.num - 1)
-        else -> throw TypeError("sub1 expected Num")
-    }
-}
-
-val mul = Fn("*") { exprs, env ->
-    LispNumber(getInts(exprs, env).fold(1) { a, b -> a * b })
-}
 
 val undefinable = setOf(clear.name, definitions.name)
 val define = Fn("Define") { exprs, env ->
@@ -60,4 +26,3 @@ val define = Fn("Define") { exprs, env ->
         throw TypeError("Define expects first arg to be an identifier")
     }
 }
-
