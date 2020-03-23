5782b8679358828f16abb3f3d689a168a4fb1516
Gabe <gabesaba@gmail.com>
2020-03-22 22:52:27 -0400

    CoffeeLisp: Define type type

--- src/main/kotlin/coffeebot/commands/lisp/Interpreter.kt
+++ src/main/kotlin/coffeebot/commands/lisp/Interpreter.kt
@@ -2,14 +2,14 @@ package coffeebot.commands.lisp
 
 import coffeebot.commands.Command
 
-fun interpret(s: String): String {
-    return parseLisp(s).eval(globalEnv).display()
+fun interpret(s: String): LispObject {
+    return parseLisp(s).eval(globalEnv)
 }
 
 val lisp = Command("!cl", "Eval a CoffeeLisp Expression") {
     try {
         val expr = it.contents.removePrefix("!cl")
-        it.reply("$expr -> ${interpret(expr)}")
+        it.reply("$expr -> ${interpret(expr).display()}")
     } catch (e: Exception) {
         it.reply("$e")
     }
--- src/main/kotlin/coffeebot/commands/lisp/LispObject.kt
+++ src/main/kotlin/coffeebot/commands/lisp/LispObject.kt
@@ -1,23 +1,23 @@
 package coffeebot.commands.lisp
 
 sealed class LispObject {
-    abstract fun type(): String
+    abstract fun type(): LispType
 
     abstract fun display(): String
 }
 
-class Number(val num: Int): LispObject() {
+data class Number(val num: Int): LispObject() {
     override fun display(): String {
         return num.toString()
     }
 
-    override fun type(): String = "Number"
+    override fun type() = LispType("Number")
 }
 
 class Fn(private val name: String, private val fn: (List<Expression>, Env) -> LispObject): LispObject() {
     fun apply(expressions: List<Expression>, env: Env) = fn(expressions, env)
 
-    override fun type(): String = "Fn"
+    override fun type() = LispType("Fn")
 
     override fun display() = "The Fn $name"
 }
@@ -27,15 +27,22 @@ class Identifier(private val s: String): LispObject() {
         fun isIdentifier(s: String) = s.matches("[a-zA-Z][A-Za-z0-9]*".toRegex())
     }
 
-    override fun type() = "Identifier"
+    override fun type() = LispType("Identifier")
 
     override fun display() = s
 }
 
-class LispString(private val s: String): LispObject() {
-    override fun type() = "String"
+object LispUnit: LispObject() {
+    override fun type() = LispType("Unit")
 
-    override fun display() = s
+    override fun display() = "()"
 }
+
+data class LispType(private val name: String): LispObject() {
+    override fun type() = LispType("Type")
+
+    override fun display() = name
+}
+
 
 class TypeError(override val message: String): Exception()
