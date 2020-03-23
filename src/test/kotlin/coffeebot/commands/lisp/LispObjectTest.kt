d69f87c1b875c89aac7a90aa95ee7c38d1802e89
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-03-23 03:16:16 -0400

    Fix Bug: Subtraction implemented wrongly

--- src/test/kotlin/coffeebot/commands/lisp/LispObjectTest.kt
+++ src/test/kotlin/coffeebot/commands/lisp/LispObjectTest.kt
@@ -7,10 +7,10 @@ import kotlin.test.assertTrue
 class LispObjectTest {
     @Test
     fun testIdentifier() {
-        assertTrue((Identifier.isIdentifier("hello")))
-        assertTrue((Identifier.isIdentifier("World")))
-        assertTrue((Identifier.isIdentifier("g")))
-        assertTrue((Identifier.isIdentifier("g8")))
-        assertFalse((Identifier.isIdentifier("2go")))
+        assertTrue((Token("hello").isIdentifier()))
+        assertTrue((Token("World").isIdentifier()))
+        assertTrue((Token("g").isIdentifier()))
+        assertTrue((Token("g8").isIdentifier()))
+        assertFalse((Token("2go").isIdentifier()))
     }
 }
