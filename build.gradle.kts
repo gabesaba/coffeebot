5fb12d184a5e912a07f340247bb677bcbe157567
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-04-19 19:44:48 -0700

    Import CoffeeLisp from https://github.com/gabesaba/coffeelisp

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ dependencies {
 
     implementation("com.discord4j:discord4j-core:3.0.13")
 
+    implementation("org.gabe.coffee:coffeelisp:1.0") {
+        version {
+            branch = "coffeebot"
+        }
+    }
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
