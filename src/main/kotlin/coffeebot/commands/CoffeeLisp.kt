e4bb846cd5ec373812c4d3b1aeb9fbfdf1f25b44
Gabe <15304068+gabesaba@users.noreply.github.com>
2020-04-20 13:58:21 -0700

    Switch back to shared env

--- src/main/kotlin/coffeebot/commands/CoffeeLisp.kt
+++ src/main/kotlin/coffeebot/commands/CoffeeLisp.kt
@@ -1,23 +1,18 @@
 package coffeebot.commands
 
-import coffeebot.message.User
-import coffeelisp.env.Env
 import coffeelisp.env.createEnv
 import coffeelisp.env.eval
+
+
+// Shared env is more fun, as everyone
+// can share definitions
+private val sharedEnv = createEnv()
 
 val lisp = Command("!cl", "Eval a CoffeeLisp Expression") {
     try {
         val lisp = it.contents.removePrefix("!cl")
-        val env = it.user.getEnv()
-
-        it.reply(lisp.eval(env))
+        it.reply(lisp.eval(sharedEnv))
     } catch (e: Exception) {
         it.reply("$e")
     }
-}
-
-private val envs = mutableMapOf<User, Env>()
-
-private fun User.getEnv(): Env {
-    return envs.getOrPut(this) { createEnv() }
 }
